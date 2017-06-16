c64c87ee9374b3824b1d92b0ca8e0807000c5a39
Clayton Samson <csamso1@lsu.edu>
2017-06-16 13:45:03 -0500

    Added Support for Random Operations

--- Worksheet_Generator/newDataWorksheetGenerator.java
+++ Worksheet_Generator/newDataWorksheetGenerator.java
@@ -8,10 +8,11 @@ public class newDataWorksheetGenerator
         Scanner scan = new Scanner(System.in);
     	System.out.printf("Enter the higest number to use: \n");
     	int max = scan.nextInt() - 1;
-    	System.out.printf("Enter the operation to use: \n");
+    	System.out.printf("Enter the operation to use (enter 'rand' or 'r' for random operations: \n");
         scan.nextLine();
         String operator = scan.nextLine();
         String operator_String = (String)operator;
+        String[] operations = {"+", "-", "x", "%"};
         if("*".equalsIgnoreCase(operator) || "x".equalsIgnoreCase(operator)){
             operator_String = "multiplication";
             operator = "x";
@@ -25,9 +26,9 @@ public class newDataWorksheetGenerator
         else if("-".equalsIgnoreCase(operator)){
             operator_String = "subtraction";
         }
-        // else if("r".equalsIgnoreCase(operator)){
-        //     operator_String = "Random";
-        // }
+        else if("r".equalsIgnoreCase(operator) || "rand".equalsIgnoreCase(operator)){
+            operator_String = "Random";
+        }
         System.out.printf("operator = %s\n", operator);
         System.out.printf("operator_String = %s\n", operator_String);
         // scan.nextLine();
@@ -35,15 +36,29 @@ public class newDataWorksheetGenerator
         int limit = max + 1;
         String fileName = operator_String.concat("_") + limit + "_vertical.csv";
         PrintWriter fileWriter = new PrintWriter("/mnt/c/Users/Clayton/Desktop/WorksheetGenerator/".concat(fileName));
-        for(int i = 0; i < 5; i++){
+        if("Random".equalsIgnoreCase(operator_String)){
+            for(int i = 0; i < 5; i++){
+            fileWriter.printf(",%d,,,%d,,,%d,,,%d,,,%d\n",rand.nextInt(max) + 1, rand.nextInt(max) + 1, rand.nextInt(max) + 1, rand.nextInt(max) + 1, rand.nextInt(max) + 1);
+            fileWriter.printf("%s,%d,,%s,%d,,%s,%d,,%s,%d,,%s,%d\n",operations[rand.nextInt(4)], rand.nextInt(max) + 1, operations[rand.nextInt(4)], rand.nextInt(max) + 1, operations[rand.nextInt(4)], rand.nextInt(max) + 1, operations[rand.nextInt(4)], rand.nextInt(max) + 1, operations[rand.nextInt(4)], rand.nextInt(max) + 1);
+            fileWriter.printf("\n\n");
+            }
+            for(int i = 0; i < 7; i++){
+                fileWriter.printf(",%d,,,%d,,,%d,,,%d,,,%d,,,%d,,,%d,,,%d,,,%d,,,%d\n", rand.nextInt(max) + 1, rand.nextInt(max) + 1, rand.nextInt(max) + 1, rand.nextInt(max) + 1, rand.nextInt(max) + 1, rand.nextInt(max) + 1, rand.nextInt(max) + 1, rand.nextInt(max) + 1, rand.nextInt(max) + 1, rand.nextInt(max) + 1);
+                fileWriter.printf("%s,%d,,%s,%d,,%s,%d,,%s,%d,,%s,%d,,%s,%d,,%s,%d,,%s,%d,,%s,%d,,%s,%d\n", operations[rand.nextInt(4)], rand.nextInt(max) + 1, operations[rand.nextInt(4)], rand.nextInt(max) + 1, operations[rand.nextInt(4)], rand.nextInt(max) + 1, operations[rand.nextInt(4)], rand.nextInt(max) + 1, operations[rand.nextInt(4)], rand.nextInt(max) + 1, operations[rand.nextInt(4)], rand.nextInt(max) + 1, operations[rand.nextInt(4)], rand.nextInt(max) + 1, operations[rand.nextInt(4)], rand.nextInt(max) + 1, operations[rand.nextInt(4)], rand.nextInt(max) + 1, operations[rand.nextInt(4)], rand.nextInt(max) + 1);
+                fileWriter.printf("\n\n");
+            }
+        }
+        else{
+            for(int i = 0; i < 5; i++){
             fileWriter.printf(",%d,,,%d,,,%d,,,%d,,,%d\n",rand.nextInt(max) + 1, rand.nextInt(max) + 1, rand.nextInt(max) + 1, rand.nextInt(max) + 1, rand.nextInt(max) + 1);
             fileWriter.printf("%s,%d,,%s,%d,,%s,%d,,%s,%d,,%s,%d\n",operator, rand.nextInt(max) + 1, operator, rand.nextInt(max) + 1, operator, rand.nextInt(max) + 1, operator, rand.nextInt(max) + 1, operator, rand.nextInt(max) + 1);
             fileWriter.printf("\n\n");
-        }
-        for(int i = 0; i < 7; i++){
-            fileWriter.printf(",%d,,,%d,,,%d,,,%d,,,%d,,,%d,,,%d,,,%d,,,%d,,,%d\n", rand.nextInt(max) + 1, rand.nextInt(max) + 1, rand.nextInt(max) + 1, rand.nextInt(max) + 1, rand.nextInt(max) + 1, rand.nextInt(max) + 1, rand.nextInt(max) + 1, rand.nextInt(max) + 1, rand.nextInt(max) + 1, rand.nextInt(max) + 1);
-            fileWriter.printf("%s,%d,,%s,%d,,%s,%d,,%s,%d,,%s,%d,,%s,%d,,%s,%d,,%s,%d,,%s,%d,,%s,%d\n", operator, rand.nextInt(max) + 1, operator, rand.nextInt(max) + 1, operator, rand.nextInt(max) + 1, operator, rand.nextInt(max) + 1, operator, rand.nextInt(max) + 1, operator, rand.nextInt(max) + 1, operator, rand.nextInt(max) + 1, operator, rand.nextInt(max) + 1, operator, rand.nextInt(max) + 1, operator, rand.nextInt(max) + 1);
-            fileWriter.printf("\n\n");
+            }
+            for(int i = 0; i < 7; i++){
+                fileWriter.printf(",%d,,,%d,,,%d,,,%d,,,%d,,,%d,,,%d,,,%d,,,%d,,,%d\n", rand.nextInt(max) + 1, rand.nextInt(max) + 1, rand.nextInt(max) + 1, rand.nextInt(max) + 1, rand.nextInt(max) + 1, rand.nextInt(max) + 1, rand.nextInt(max) + 1, rand.nextInt(max) + 1, rand.nextInt(max) + 1, rand.nextInt(max) + 1);
+                fileWriter.printf("%s,%d,,%s,%d,,%s,%d,,%s,%d,,%s,%d,,%s,%d,,%s,%d,,%s,%d,,%s,%d,,%s,%d\n", operator, rand.nextInt(max) + 1, operator, rand.nextInt(max) + 1, operator, rand.nextInt(max) + 1, operator, rand.nextInt(max) + 1, operator, rand.nextInt(max) + 1, operator, rand.nextInt(max) + 1, operator, rand.nextInt(max) + 1, operator, rand.nextInt(max) + 1, operator, rand.nextInt(max) + 1, operator, rand.nextInt(max) + 1);
+                fileWriter.printf("\n\n");
+            }
         }
         fileWriter.close();
         System.out.printf("%s created!\n", fileName);
